734c950fdb354654d60893be994a9ef33ea56687
Linpn <lpn5200@qq.com>
2017-02-06 10:10:35 +0800

    Merge branch 'ibatis-2.3.4.726'

--- ibator/bin/run.sh
+++ ibator/bin/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Dfile.encoding=UTF-8 -jar ibator-generator-1.3.2.jar -configfile config.xml -overwrite 
+java -Dfile.encoding=UTF-8 -jar ibator-1.3.2.jar -configfile config.xml -overwrite 
